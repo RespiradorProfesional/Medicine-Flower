56bc831b196e38a8c21e55915b8ba585490e5ba3
Néstor Álvarez <116906280+Nstorlvarez@users.noreply.github.com>
2026-03-29 10:32:49 +0100

    final

--- scenes/puzzle_1.gd
+++ scenes/puzzle_1.gd
@@ -5,14 +5,16 @@ extends PuzzleBase
 @export var dialogue_resource_2 : DialogueResource 
 
 var ovejas_count=0
+var already_finished=false
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is OvejaSimple:
 		ovejas_count+=1
 		print(ovejas_count)
-		if ovejas_count>=ovejas_total:
+		if ovejas_count>=ovejas_total and not already_finished:
 			print("finish")
 			finished_puzzle()
+			already_finished=true
 
 func finished_puzzle() ->void:
 	colision_invisible_wall.set_deferred("disabled", true)
